a6a70057ac335ea0b3272e8bd13ef231ce765a5d
Edixon Moreno <edixon.moreno.fuentes@gmail.com>
2021-12-16 21:12:00 -0400

    first commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import { useEffect } from "react/cjs/react.development";
+import {getGifs} from '../helpers/getGifs'
+
+
+export const useFetchGif = ( category ) => {
+
+    const [state, setState] = useState({
+        data: [],
+        loading: true
+    });
+
+    useEffect(  () => {
+
+        getGifs( category )
+            .then(imgs => {
+             
+                     setState({
+                        data: imgs,
+                        loading: false
+                    }); 
+                })
+
+    }, [category])
+
+    return state;
+
+}
+
